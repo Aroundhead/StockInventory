d5036bfa7528fd3e17abbae5aa7a8fed5c4d22b9
Aroundhead <a219207415@unison.mx>
2024-11-21 13:00:21 -0700

    ya mero

--- config/db.js
+++ config/db.js
@@ -1,28 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();  // Cargar variables de entorno desde .env
-
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_NAME);
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 100, // Número máximo de conexiones en el pool
-  queueLimit: 0
+  port: process.env.DB_PORT
 });
 
-pool.getConnection((err, connection) => {
+connection.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err.stack);
     return;
   }
   console.log('Conectado a la base de datos.');
-  connection.release(); // Liberar la conexión de vuelta al pool
 });
 
-module.exports = pool;
+module.exports = connection;
