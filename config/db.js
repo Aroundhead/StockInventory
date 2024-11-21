49144c8dbc64495edc96050760c98e7f12e5512b
Aroundhead <a219207415@unison.mx>
2024-11-21 12:52:40 -0700

    connection with pool

--- config/db.js
+++ config/db.js
@@ -1,23 +1,28 @@
 const mysql = require('mysql2');
 require('dotenv').config();  // Cargar variables de entorno desde .env
+
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_NAME);
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 100, // Número máximo de conexiones en el pool
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err.stack);
     return;
   }
   console.log('Conectado a la base de datos.');
+  connection.release(); // Liberar la conexión de vuelta al pool
 });
 
-module.exports = connection;
+module.exports = pool;
